72f6e3b7fd77c99cc5d3454623e52e0d610277d5
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2020-02-13 13:12:09 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,34 +3,34 @@ const { mapResults } = require('../helper')
 const { isEqual } = require('lodash')
 
 const nodePingResult = [{
-  '_id': '1111',
-  's': 1111,
-  'e': 1111,
-  'rt': 18,
-  'l': { '1111': 'ld' },
-  'tg': 'https://xxxx.com',
+  '_id': '1',
+  's': 1,
+  'e': 1,
+  'rt': 1,
+  'l': { '1': 'ld' },
+  'tg': 'https://s.com',
   'i': 1,
   'su': true
 }]
 const mapped = {
-  id: '1111',
-  start: 1111,
-  end: 1111,
-  duration: 18,
-  locations: '{ \'1111\': \'ld\' }',
-  target: 'https://xxxx.com',
+  id: '1',
+  start: 1,
+  end: 1,
+  duration: 1,
+  locations: '{"1":"ld"}',
+  target: 'https://s.com',
   interval: 1,
   success: true
 }
 
 describe('helper', function () {
   describe('#mapResults()', function () {
-    it('result should be array', function (done) {
+    it('result should be array', function () {
       assert.ok(Array.isArray(mapResults(nodePingResult)))
     })
 
     it('should convert nodeping result to human readable format', function () {
       assert.ok(isEqual(mapResults(nodePingResult)[0], mapped))
-    })
-  })
-})
+    });
+  });
+});
